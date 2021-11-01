8c7b13c6095e56daf563829f7551528f3f78286d
JohanvandenHeuvel <johanheuvel5@gmail.com>
2021-11-01 12:46:40 +0100

    implemented more measures and get results now

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
-from scipy.optimize import minimize
-
-from sklearn.metrics import accuracy_score
+import numpy as np
+from numpy.linalg import norm
+from scipy.optimize import minimize, LinearConstraint
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.model_selection import KFold
 
 from load_data import load_adult, load_toy_test
-from losses import *
 
 
-def Expectation(losses: np.ndarray, subgroups: list) -> float:
-    """Expectation
-    Loss aggregator, E[X]
-
-    :param losses: losses to aggregate over
-    :param subgroups: list of list filled with indices of the subgroup
-    :return: aggregated losses
-    """
-
-    subgroup_losses = [losses[mask] for mask in subgroups]
-
-    # compute the subgroup mean losses
-    subgroup_mean_losses = np.zeros(len(subgroup_losses))
-    for i, subgroup_loss in enumerate(subgroup_losses):
-        # calculate the expected loss over the subgroup
-        # and take the square (squared hinge loss)
-        mean = np.mean(subgroup_loss) ** 2
-        subgroup_mean_losses[i] = mean
-
-    return np.mean(subgroup_mean_losses)
+#################
+# loss function #
+#################
+def hinge_loss(actual, predicted):
+    loss = np.array(1 - np.multiply(actual, predicted)).clip(0)
+    return loss
 
 
+def square_hinge_loss(actual, predicted):
+    return hinge_loss(actual, predicted) ** 2
+
+
+####################
+# loss aggregators #
+####################
 def CVaR(
     losses: np.ndarray, subgroups: list, p: float, alpha: float, clip: bool = True
 ) -> float:
@@ -56,6 +50,9 @@ def CVaR(
     return p + 1 / (1 - alpha) * np.mean(subgroup_mean_losses)
 
 
+#####################
+# linear classifier #
+#####################
 def predict(w, x, threshold=False):
     y_hat = x @ np.expand_dims(w, 1)
 
@@ -77,11 +74,13 @@ def regularised_linear_scorer(w, x, y, loss, lmbd=0.1):
     """
     y_pred = predict(w, x)  # linear scorer
     regularisation = norm(w, 2)  # L2 regularization
-    return loss(y, y_pred)
+    return loss(y, y_pred) + lmbd * regularisation
 
 
-def main():
-
+########
+# data #
+########
+def get_data():
     # load the data
     train_data, test_data = load_adult()
 
@@ -93,38 +92,131 @@ def main():
     y_test = test_data.target
     y_test = np.expand_dims(y_test, 1)
 
+    return x_train, y_train, x_test, y_test
+
+
+# def constraint():
+#     # constraints = (LinearConstraint(np.eye(len(mask), 0, np.inf)))
+#     params[-1] + 1 / (1 - a) * np.mean(subgroup_loss_aggregation(
+#         regularised_linear_scorer(
+#             params[:-1], x=x_train, y=y_train, loss=hinge_loss
+#         ),
+#         subgroups=mask,
+#         p=params[-1],
+#     pass
+#     f = lambda params: np.sum(params)
+#     # set constraints
+#     cons = (
+#         {"type": "ineq", "fun": lambda w: w[:-1]},
+#         {
+#             "type": "ineq",
+#             "fun": lambda w: CVaR(
+#                 regularised_linear_scorer(w[:-1], x_train, y_train, square_hinge_loss),
+#                 subgroups=mask,
+#                 p=w[-1],
+#                 alpha=a
+#             ),
+#         },
+#     )
+
+
+def optimize(x, y, alpha, lmbda, verbose=False):
+    # get the indices of the groups induced by the sensitive feature
     sensitive_feature = 9
-    sensitive_feature_values = np.unique(x_train[:, sensitive_feature])
+    sensitive_feature_values = np.unique(x[:, sensitive_feature])
     mask = []
     for i, value in enumerate(sensitive_feature_values):
-        mask.append(x_train[:, sensitive_feature] == value)
+        mask.append(x[:, sensitive_feature] == value)
+
+    x0 = np.zeros(x.shape[1] + 1)
+    f = lambda params: CVaR(
+        regularised_linear_scorer(params[:-1], x, y, square_hinge_loss, lmbd=lmbda),
+        subgroups=mask,
+        p=params[-1],
+        alpha=alpha,
+    )
+    if verbose:
+        print(f"--> Minimizing the objective for alpha={alpha} and lambda={lmbda} ...")
+    return minimize(f, x0, options={"disp": verbose})
+
+
+def main(lambdas=None):
+    if lambdas is None:
+        lambdas = []
+
+    x_train, y_train, x_test, y_test = get_data()
 
     # find optimal solution
-    x0 = np.zeros(x_train.shape[1])
-    # f = lambda w: Expectation(
-    #     regularised_linear_scorer(w, x=x_train, y=y_train, loss=hinge_loss),
-    #     subgroups=mask,
-    # )
-    f = lambda w: CVaR(
-        regularised_linear_scorer(w, x=x_train, y=y_train, loss=hinge_loss),
-        subgroups=mask, p=0.0, alpha=0.1
-    )
-    print("Minimizing the objective...")
-    result = minimize(
-        f,
-        x0,
-        options={"disp": True, "return_all": True},
-    )
+    optimal_lambdas = []
+    for i, a in enumerate(np.arange(1, 10) / 10):
 
-    print(
-        f"Training data accuracy: {accuracy_score(y_train, predict(result.x, x_train, threshold=True)):0.2f}"
-    )
-    print(
-        f"Test data accuracy: {accuracy_score(y_test, predict(result.x, x_test, threshold=True)):0.2f}"
-    )
+        if len(lambdas) == i:
+            best_lambda = 0
+            best_score = 0
+            for l in np.arange(0, 11) / 10:
+                kf = KFold(n_splits=5)
+                scores = []
+                for train, validation in kf.split(x_train):
+                    x = x_train[train]
+                    y = y_train[train]
+                    x_val = x_train[validation]
+                    y_val = y_train[validation]
 
-    print("Done!")
+                    result = optimize(x, y, a, l)
+
+                    w = result.x[:-1]
+                    scores.append(
+                        balanced_accuracy_score(
+                            y_val, predict(w, x_val, threshold=True)
+                        )
+                    )
+                if np.mean(scores) > best_score:
+                    best_score = np.mean(scores)
+                    best_lambda = l
+            lambdas.append(best_lambda)
+
+        print(f"Found optimal lambdas: {lambdas} for alphas {np.arange(1, 10)/10}")
+
+        l = lambdas[i]
+        result = optimize(x_train, y_train, alpha=a, lmbda=l, verbose=True)
+
+        w = result.x[:-1]
+        p = result.x[-1]
+
+        # get the indices of the groups induced by the sensitive feature
+        sensitive_feature = 9
+        sensitive_feature_values = np.unique(x_train[:, sensitive_feature])
+        mask_train = []
+        mask_test = []
+        for value in sensitive_feature_values:
+            mask_train.append(
+                np.logical_and(
+                    x_train[:, sensitive_feature] == value, y_train.flatten() == 1
+                )
+            )
+            mask_test.append(
+                np.logical_and(
+                    x_test[:, sensitive_feature] == value, y_test.flatten() == 1
+                )
+            )
+
+        print(f"p: {p}")
+        y_predict = predict(w, x_train, threshold=True)
+        print(
+            f"Training data balanced accuracy: {balanced_accuracy_score(y_train, y_predict):0.2f}"
+        )
+        print(
+            f"Training data equality of opportunity: {np.abs(np.mean(y_predict[mask_train[0]]) - np.mean(y_predict[mask_train[1]]))}"
+        )
+        y_predict = predict(w, x_test, threshold=True)
+        print(
+            f"Test data balanced accuracy: {balanced_accuracy_score(y_test, y_predict):0.2f}"
+        )
+        print(
+            f"Test data equality of opportunity: {np.abs(np.mean(y_predict[mask_test[0]]) - np.mean(y_predict[mask_test[1]]))}"
+        )
+        print("<-- Done!")
 
 
 if __name__ == "__main__":
-    main()
+    main(lambdas=[0.1] * 9)
